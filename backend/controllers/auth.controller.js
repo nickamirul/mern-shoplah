007223a90cd65a43f732149afa92aba7253e93d9
nickamirul <nickamirul9945@gmail.com>
2025-02-23 16:19:20 +0800

    Create Sign Up API Route

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+import User from '../models/user.model.js'
+
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const newUser = new User({ username, email, password: hashedPassword })
+    try {
+        await newUser.save()
+        res.status(201).json("user created successfully")
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+ }
+ 
--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
-
+import authRoutes from './routes/auth.route.js'
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_URI)
@@ -17,9 +17,11 @@ mongoose.connect(process.env.MONGO_URI)
 const app = express()
 
 app.use(cors())
+app.use(express.json())
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
 
-app.use('/api/user', userRoutes)
+app.use('/v1/user', userRoutes)
+app.use('/v1/auth', authRoutes)
