32c343e6e570fa51697cc5436b3ab1cf21ff0dd7
nickamirul <nickamirul9945@gmail.com>
2025-02-23 15:00:53 +0800

    add shadcn ui package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+
 function App() {
   return (
     <>
       <h1 className="text-3xl text-red-500 font-bold underline">
         Hello world!
       </h1>
+      <Button className="bg-red-500 text-white">Click me</Button>
     </>
   )
 }
